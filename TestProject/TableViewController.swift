387462cc502f9ec8fe9c71d3cbda3ad49ba84b63
Vlad Maevsky <vlad.maevsky@yandex.ru>
2019-01-07 20:06:10 +0300

    Completed LT-02

--- TestProject/TableViewController.swift
+++ TestProject/TableViewController.swift
@@ -15,6 +15,7 @@ class TableViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         self.navigationItem.leftBarButtonItem = self.editButtonItem
+        addButtonCellOutlet.isHidden = true
     }
 
     override func numberOfSections(in tableView: UITableView) -> Int {
@@ -30,14 +31,44 @@ class TableViewController: UITableViewController {
         cell.textLabel?.text = items[indexPath.row]
         return cell
     }
+    
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    
+    override func tableView(_ tableView: UITableView, moveRowAt sourceIndexPath: IndexPath, to destinationIndexPath: IndexPath) {
+        let item = items[sourceIndexPath.row]
+        items.remove(at: sourceIndexPath.row)
+        items.insert(item, at: destinationIndexPath.row)
+    }
 
     @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        addButtonAction()
+    }
+    
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        items.remove(at: indexPath.row)
+        tableView.beginUpdates()
+        tableView.deleteRows(at: [indexPath], with: .automatic)
+        tableView.endUpdates()
+    }
+    
+    
+    @IBAction func addButtonCell(_ sender: UIButton) {
+        addButtonAction()
+    }
+    @IBOutlet weak var addButtonCellOutlet: UIButton!
+    
+    func addButtonAction() {
         var textField: UITextField?
         
         let alert = UIAlertController(title: "Add New Category", message: "", preferredStyle: .alert)
         let action = UIAlertAction(title: "Add", style: .default) { [unowned self] _ in
             self.items.append(textField?.text ?? "")
-            self.tableView.reloadData()
+            let indexPath = IndexPath(row: self.items.count - 1, section: 0)
+            self.tableView.beginUpdates()
+            self.tableView.insertRows(at: [indexPath], with: .automatic)
+            self.tableView.endUpdates()
         }
         
         alert.addTextField { alertTextField in
@@ -49,8 +80,13 @@ class TableViewController: UITableViewController {
         present(alert, animated: true, completion: nil)
     }
     
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        items.remove(at: indexPath.row)
-        tableView.reloadData()
+    override func setEditing(_ editing: Bool, animated: Bool) {
+        if (editing) {
+            super.setEditing(true, animated: true)
+            addButtonCellOutlet.isHidden = false
+        } else {
+            super.setEditing(false, animated: true)
+            addButtonCellOutlet.isHidden = true
+        }
     }
 }
